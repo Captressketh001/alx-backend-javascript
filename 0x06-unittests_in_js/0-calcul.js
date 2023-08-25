861a5acbc571b43c3c893125365a5c09b71efbde
Oluwakemi Omoyeni <shininglite2012@gmail.com>
2023-08-25 17:02:33 +0100

    added test

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -2,5 +2,7 @@ const calculateNumber = (a, b) => {
     const a_round = Math.round(a)
     const b_round = Math.round(b)
 
-    return a_round+b_round
+    return a_round + b_round
 }
+
+module.exports = calculateNumber;
