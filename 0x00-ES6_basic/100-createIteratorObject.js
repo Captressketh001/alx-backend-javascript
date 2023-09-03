630ebe77ca641916e2690b895ac76a5807b1e065
Oluwakemi Omoyeni <shininglite2012@gmail.com>
2023-09-03 21:35:00 +0100

    corrections

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,3 +1,9 @@
-export default function iterateThroughObject(reportWithIterator) {
-
+export default function iterateThroughObject(report) {
+    return (function* _() {
+        for (const department of Object.values(report.allEmployees)) {
+            for (const employee of department){
+                yield employee;
+            }
+        }
+    }());
 }
