a8a9f78dcdaa16846616c8a8d38f6fa2c23ad2db
Captressketh001 <shininglite2012@gmail.com>
2023-09-17 21:07:22 +0100

    correction

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -24,7 +24,7 @@ export default class HolbertonClass {
     if (hint === 'number') {
       return this.size;
     }
-    if (hint === 'location') {
+    if (hint === 'string') {
       return this.location;
     }
     return this;
